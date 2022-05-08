f96d2d155f1af2069ecc8b8132d92e4e3c2c6d18
minhvo1 <minhvo199@gmail.com>
2022-05-08 20:50:02 +0000

    completed index.js

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
-
 function sum(fromN, toN) {
-  // Sum all the values from fromN up to toN
-}
+  if (fromN < toN) {
+    return toN + sum(fromN, toN - 1);
+  } else {
+    return fromN;
+  }
+};
 
 module.exports = sum;
